230409364c2e8cca1aa991f0cabf4f31f38264e4
Kevin Yu <thekevinyu@gmail.com>
2021-06-22 01:04:52 -0700

    Add block viewer panel

---
+++ soundsep/plugins/block_viewer.py
@@ -0,0 +1,21 @@
+"""A template for a new plugin
+
+Any methods here can be deleted and will use the default implementations
+found in soundsep.core.base_plugin
+"""
+from PyQt5 import QtWidgets as widgets
+from soundsep.core.base_plugin import BasePlugin
+from soundsep.widgets.block_view import AudioFileView
+
+
+
+class BlockViewer(BasePlugin):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.view = AudioFileView()
+        self.view.show_columns(["Name", "Rate", "Ch", "Dur"])
+        self.view.set_blocks(self.api.project.blocks)
+
+    def plugin_panel_widget(self) -> 'PyQt5.QtWidgets.QWidget':
+        return self.view
